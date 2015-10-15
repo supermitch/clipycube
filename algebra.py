721f18da4d38ac76171165596bc11e2572c60204
Mitch LeBlanc <supermitch@gmail.com>
2015-10-14 21:13:59 -0700

    Fix bug where vector calculations returned Ints only

--- algebra.py
+++ algebra.py
@@ -9,17 +9,17 @@ def rotation(point, axis, sign=1):
     Rotate a point (or vector) about the origin in 3D space.
     """
     def Rx(x, y, z, theta):
-        return (int(x),
-                int(math.cos(theta) * y - math.sin(theta) * z),
-                int(math.sin(theta) * y + math.cos(theta) * z))
+        return (round(x, 1),
+                round(math.cos(theta) * y - math.sin(theta) * z, 1),
+                round(math.sin(theta) * y + math.cos(theta) * z, 1))
     def Ry(x, y, z, theta):
-        return (int(math.cos(theta) * x + math.sin(theta) * z),
-                int(y),
-                int(-math.sin(theta) * x + math.cos(theta) * z))
+        return (round(math.cos(theta) * x + math.sin(theta) * z, 1),
+                round(y, 1),
+                round(-math.sin(theta) * x + math.cos(theta) * z, 1))
     def Rz(x, y, z, theta):
-        return (int(math.cos(theta) * x - math.sin(theta) * y),
-                int(math.sin(theta) * x + math.cos(theta) * y),
-                int(z))
+        return (round(math.cos(theta) * x - math.sin(theta) * y, 1),
+                round(math.sin(theta) * x + math.cos(theta) * y, 1),
+                round(z, 1))
     R = {'x': Rx, 'y': Ry, 'z': Rz}[axis]  # Select a rotation matrix
     theta = sign * math.pi / 2  # Always 90 degrees
     x, y, z = point  # Assumes 3D point or vector
