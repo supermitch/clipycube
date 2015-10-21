28e9bf7c4c61b12c6776d03a5755ccaaf8d2ddf8
Mitch LeBlanc <supermitch@gmail.com>
2015-10-20 21:18:49 -0700

    Add code for direction cosines

--- algebra.py
+++ algebra.py
@@ -5,9 +5,7 @@ import math
 
 
 def rotation(point, axis, sign=1):
-    """
-    Rotate a point (or vector) about the origin in 3D space.
-    """
+    """ Rotate a point (or vector) about the origin in 3D space. """
     def Rx(x, y, z, theta):
         return (round(x, 1),
                 round(math.cos(theta) * y - math.sin(theta) * z, 1),
@@ -28,11 +26,23 @@ def rotation(point, axis, sign=1):
     x, y, z = point  # Assumes 3D point or vector
     return R(x, y, z, theta)  # Calculate our new normal vector
 
+
 def cross_product(vector_a, vector_b):
-    """
-    Return the angles in (x, y, z) between two vectors.
-    """
+    """ Return the angles in (x, y, z) between two vectors. """
     a1, a2, a3 = vector_a
     b1, b2, b3 = vector_b
     return (a2 * b3 - a3 * b2, a3 * b1 - a1 * b3, a1 * b2 - a2 * b1)
 
+
+def length(vector):
+    """ Return length of a vector. """
+    return math.sqrt(a ** 2 + b ** 2 + c ** 2)
+
+
+def direction_cosines(vector):
+    """ Returns direction cosines for a given vector. """
+    a, b, c = vector
+    alpha = math.acos(a/length(vector))
+    beta = math.acos(b/length(vector))
+    gamma = math.acos(c/length(vector))
+    return alpha, beta, gamma
